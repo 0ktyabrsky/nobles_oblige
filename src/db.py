cb3174bdc816ad5209ff31ffb415a27512340133
Oktybrsky <rinatshailoobekov@gmail.com>
2026-03-25 18:22:43 +0600

    fix: remove supabase from files

--- src/db.py
+++ src/db.py
@@ -1,4 +1,4 @@
-from supabase import create_client , acreate_client
+
 
 URL = 'https://ltmyyctdzbibbfpughdt.supabase.co'
 KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0bXl5Y3RkemJpYmJmcHVnaGR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4NTQxNTksImV4cCI6MjA4NzQzMDE1OX0.TaJLUSwm-PwrfeZ8pG25OXIgRLNFFiiJU87J1wF7noI'
